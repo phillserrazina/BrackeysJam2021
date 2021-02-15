8fa5baacf883ee3fc6ac03b13d0537d8df646e6e
Phill <fserrazina99@gmail.com>
2021-02-15 16:55:27 +0000

    Basic Scene Transition

---
+++ Assets/Scripts/Core/Entities/PlayerBattleRecruitManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Linq;
+
+namespace BrackeysJam.Core.Entities
+{
+    public class PlayerBattleRecruitManager : MonoBehaviour
+    {
+        public IEnumerable<RecruitableTypes> RecruitTypes {
+            get {
+                return Enum.GetValues(typeof(RecruitableTypes)).Cast<RecruitableTypes>();
+            } 
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            foreach (var type in RecruitTypes) {
+                Debug.Log("Should spawn " + PlayerPrefs.GetInt($"{type.ToString()}") + " " + type.ToString());
+            }
+        }
+    }
+}
